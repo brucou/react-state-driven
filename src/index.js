3147c2552cade290ee1237ccf69c9cf05b5899e7
brucou <bruno.couriol@gmail.com>
2018-10-25 13:11:36 +0200

    chore(build)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { Machine } from "./Machine";
-import { COMMAND_RENDER, NO_ACTIONS, NO_STATE_UPDATE } from "./properties";
-import { destructureEvent, renderAction, renderCommandFactory } from "./helpers";
+export { Machine } from "./Machine";
+export { COMMAND_RENDER, NO_ACTIONS, NO_STATE_UPDATE } from "./properties";
+export { destructureEvent, renderAction } from "./helpers";
 
-export { Machine, renderCommandFactory, renderAction, destructureEvent, COMMAND_RENDER, NO_STATE_UPDATE, NO_ACTIONS };
+// export { Machine, renderAction, destructureEvent, COMMAND_RENDER, NO_STATE_UPDATE, NO_ACTIONS };
