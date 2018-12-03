4f46c6f3e15432f89a5b3067f4d10248c8e69298
brucou <bruno.couriol@gmail.com>
2018-12-03 18:53:36 +0100

    chore(test) : added test for the image gallery machine

--- tests/index.js
+++ tests/index.js
@@ -5,3 +5,8 @@
 import './image_gallery_machine.specs'
 import './image_gallery_component.specs'
 QUnit.dump.maxDepth = 20;
+
+
+// to get string version without loosing undefined through JSON conversion
+// JSON.stringify(hash, (k, v) => (v === undefined) ? '__undefined' : v)
+//   .replace('"__undefined"', 'undefined')
