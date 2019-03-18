0f8e353204fc16bc89662cc320cee232062c35be
brucou <bcouriol@sparks.network>
2019-03-19 00:38:08 +0100

    faet(API): noop

--- src/properties.js
+++ src/properties.js
@@ -1,5 +1,6 @@
 import { NO_OUTPUT } from "state-transducer";
 
+export const noop = () => {};
 export const CONTRACT_MODEL_UPDATE_FN_RETURN_VALUE = `Model update function must return valid update operations!`;
 export const ERR_COMMAND_HANDLERS = command => (`Cannot find valid executor for command ${command}`)
 export const COMMAND_RENDER = 'render';
